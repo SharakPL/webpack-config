d483de96b71973ac2df58d1b5e39066a1ae0902c
SharakPL <git@sharak.pl>
2020-06-18 23:58:45 +0200

    setup eslint

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,20 +1,26 @@
-const path = require("path")
+const path = require('path');
 
-const isProd = process.env.NODE_ENV === 'production' ? true : false;
-const isDev = process.env.NODE_ENV === 'development' ? true : false;
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = process.env.NODE_ENV === 'development';
+let mode = 'none';
+if (isProd) {
+  mode = 'production';
+} else if (isDev) {
+  mode = 'development';
+}
 
 module.exports = {
   entry: {
-    main: "./src/main.js"
+    main: './src/main.js',
   },
-  mode: isProd ? "production" : (isDev ? "development" : 'none'),
+  mode,
   output: {
-    filename: "assets/[name]-bundle.js",
+    filename: 'assets/[name]-bundle.js',
     path: path.resolve(__dirname, '../dist'),
-    publicPath: '/'
+    publicPath: '/',
   },
   devServer: {
-    contentBase: "dist"
+    contentBase: 'dist',
   },
   module: {
     rules: [
@@ -23,35 +29,35 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         use: [
           {
-            loader: "babel-loader"
-          }
-        ]
+            loader: 'babel-loader',
+          },
+        ],
       },
       {
         test: /\.css$/,
         use: [
           {
-            loader: "style-loader"
+            loader: 'style-loader',
           },
           {
-            loader: "css-loader"
-          }
-        ]
+            loader: 'css-loader',
+          },
+        ],
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].html"
-            }
+              name: '[name].html',
+            },
           },
           {
-            loader: "extract-loader"
+            loader: 'extract-loader',
           },
           {
-            loader: "html-loader",
+            loader: 'html-loader',
             options: {
               attributes: {
                 list: [
@@ -60,23 +66,23 @@ module.exports = {
                     attribute: 'src',
                     type: 'src',
                   },
-                ]
-              }
-            }
-          }
-        ]
+                ],
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.(gif|jpe?g|png)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "assets/images/[name].[ext]"
-            }
-          }
-        ]
-      }
-    ]
-  }
-}
+              name: 'assets/images/[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 import './css/main.css';
 import './index.html';
 
-const a = async (args) => {
-  let promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("done!"), 2000)
+const test = async (args) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('done!'), 2000);
   });
   console.log(promise);
-  const {a, b} = args;
+  const { a, b } = args;
 
-  let result = await promise; // wait until the promise resolves (*)
+  const result = await promise; // wait until the promise resolves (*)
 
   console.log(result, a, b); // "done!"
   console.log(promise);
-}
+};
 
-a({a: 1, b: 2});
+test({ a: 1, b: 2 });
